b374c0df05628ee0a980f2301ffd08cada65a377
JULIA CAZERI ZANATTA <julia.zanatta@cs.up.edu.br>
2025-03-20 11:43:39 -0300

    Banco de dados

---
+++ Carro/API/Models/Carro.cs
@@ -0,0 +1,11 @@
+namespace API.Models {
+    public class Carro {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CriadoEm { get; set; }
+
+        public Carro() {
+            this.CriadoEm = DateTime.Now;
+        }
+    }
+}
