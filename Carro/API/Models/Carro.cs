0e05bc0fceb339d79dd52352c1b9b93fe64b0161
JULIA CAZERI ZANATTA <julia.zanatta@cs.up.edu.br>
2025-04-03 11:20:02 -0300

    Aula 08

--- Carro/API/Models/Carro.cs
+++ Carro/API/Models/Carro.cs
@@ -3,6 +3,7 @@ namespace API.Models {
         public int Id { get; set; }
         public string Name { get; set; }
         public DateTime CriadoEm { get; set; }
+        public Modelo? Modelo { get; set; }
 
         public Carro() {
             this.CriadoEm = DateTime.Now;
