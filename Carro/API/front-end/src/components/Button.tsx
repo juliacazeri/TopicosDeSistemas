bdee13da3486e3cbd5cb6f4e4e168e6dcbcfd0e5
JULIA CAZERI ZANATTA <julia.zanatta@cs.up.edu.br>
2025-05-08 12:20:34 -0300

    Front end

---
+++ Carro/API/front-end/src/components/Button.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+function Button({texto, valor, evento} : any){
+    
+    return (
+        <button onClick={evento}>
+            {texto} {valor}
+        </button>
+    );
+}
+
+export default Button;
